8ade9191133a08a42f3cfe0315b35a7d8718d6e2
Bhaskarreddy Gurram <bhaskarreddygurram@gmail.com>
2020-05-19 23:44:48 +0200

    New Quesions

--- question.js
+++ question.js
@@ -1,28 +1,53 @@
 //Array of Objects
 const quiz = [
-    {q:'Which month comes right before june ?',
-    option: ['may','september','july','august'],
-    answer: 0
-    },
     {
-        q:'What color is a banana?',
-        option:['red','yellow','white','blue'],
-        answer:1
-    },
-    {   
-        q:'3 + 4 = 7 ?',
-        option:['true','false'],
-        answer:0
-    },
-    {
-        q:'What time of day do we have breakfast?',
-        option:['In the afternoon','In the evening','In the morning'],
+        q:'Which country held the 2016 Summer Olympics?',
+        option:['China', 'Ireland', 'Brazil', 'italy'],
         answer:2
     },
     {
-        q:'What is 22 + 6?',
-        option:['99','56','16','78'],
-        answer:3
+        q:'Which planet is the hottest?',
+        option:['Venus', 'Saturn', 'Mercury', 'Mars'],
+        answer: 0
+    },
+    {
+        q:'Who was the only U.S. President to resign?',
+        option:['Herbert Hoover','Richard Nixon','George W. Bush','Barack Obama'],
+        answer: 1
+    },
+    {
+        q:'According to Forrest Gump, “life was like…”',
+        option:['A bag of lemons','A handful of roses','A lollipop','A box of chocolates'],
+        answer: 3
+    },
+    {
+        q:'What is the rarest blood type?',
+        option:['O','A','B','AB-Negative'],
+        answer: 3
+    },
+    {
+        q:'Which one of these characters aren’t a part of the Friends group?',
+        option:['Rachel','Joey','Gunther','Monica'],
+        answer: 2
+    },
+    {
+        q:'How many bones are there in the human body?',
+        option:['206','205','201','209'],
+        answer: 0
+    },
+    {
+        q:'Which ocean is the largest?',
+        option:['Indian','Pacific','Atlantic','Arctic'],
+        answer:1
+    },
+    {
+        q:'Which New York City building is the tallest:',
+        option:['Empire State Building','Bank of America Tower','One World Trade Center','Statue of Liberty'],
+        answer:2
+    },
+    {
+        q:'Who painted the Mona Lisa?',
+        option:['Van Gogh', 'da Vinci','Picaso','Monet'],
+        answer:1
     }
-
 ]
